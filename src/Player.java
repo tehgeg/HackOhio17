d366b1000c620e859e41d036403a55e7f438d41a
tehGEG <connorcimino1@gmail.com>
2017-10-21 14:57:53 -0400

    Initial implementation of Player class.

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,33 @@
 //Class for the player of the game
 public class Player {
-
+	
+	private int playerScore;
+	private String playerName;
+	
+	//Constructor
+	Player(String name){
+		this.playerScore = 0;
+		this.playerName = name;
+	}
+	
+	//Returns the current players name
+	String getName() {
+		return this.playerName;
+	}
+	
+	//Returns the current players score
+	int getScore() {
+		return this.playerScore;
+	}
+	
+	//Adds 1 to score
+	void incrementScore() {
+		this.playerScore++;
+	}
+	
+	//Subtracts 1 from score
+	void decrementScore() {
+		this.playerScore--;
+	}
+	
 }
