102a98bd8cbf513e51ea48dbccb33f137fcd81f4
Maxwell Koster <Koster.35@osu.edu>
2017-10-21 15:29:17 -0400

    Updates to platform.java

--- src/platform.java
+++ src/platform.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.Point;
 
 /*
@@ -11,12 +13,66 @@ public class platform {
 	
 	private int width;
 	
+	private vector2D displacement;
+	
 	/*
 	 * Constructor for the platform object
 	 */
 	public void platform(int x, int y, int length, int width) {
 		point = new Point(x, y);
+		this.length = length;
+		this.width = width;
 		
+	}
+	
+	public int getX() {
+		return point.x;
+	}
+	
+	public int getY() {
+		return point.y;
+	}
+	
+	public int getLength() {
+		return this.length;
+	}
+	
+	public int getWidth() {
+		return this.width;
+	}
+	
+	public vector2D getDisplacement() {
+		return this.displacement;
+	}
+	
+	public void setX(int x) {
+		point.x = x;
+	}
+	
+	public void setY(int y) {
+		point.y = y;
+	}
+	
+	public void setLength(int length) {
+		this.length = length;
+	}
+	
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	
+	public void setDisplacement(vector2D v) {
+		this.displacement = v;
+	}
+	
+	public void	updatePos() {
+		point.x += displacement.getX();
+		point.y += displacement.getY();
+	}
+	
+	public void draw(Graphics2D g) {
+		g.setColor(Color.RED);
+		g.fillRect(getX(),getY(), getLength(), getWidth());
 	}
 	
 
