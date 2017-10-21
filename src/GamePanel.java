40922d3b9a73c81074aba0963c35ff65cedfe4ba
tehGEG <connorcimino1@gmail.com>
2017-10-21 19:33:48 -0400

    Finished rest of GamePanel.

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -3,108 +3,120 @@ import java.awt.Dimension;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements Runnable {
-
-final static int platWidth = 200;
-final static int platLength = 20;
-
-final static int plat1_InitX = 0;
-final static int plat1_InitY = 0;
-
-final static int plat2_InitX = 256;
-final static int plat2_InitY = 144;
-
-final static int plat3_InitX = 2*plat2_InitX;
-final static int plat3_InitY = 2*plat2_InitY;
-
-final static int plat4_InitX = 3*plat2_InitX;
-final static int plat4_InitY = 3*plat2_InitY;
-
-final static int plat5_InitX = 4*plat2_InitX;
-final static int plat5_InitY = 4*plat2_InitY;
 	
-final static int plat6_InitX = 5*plat2_InitX;
-final static int plat6_InitY = 5*plat2_InitY;
-
-final static int plat7_InitX = 6*plat2_InitX;
-final static int plat7_InitY = 6*plat2_InitY;
+	private static final long serialVersionUID = 1L;
 	
-private Platform plat1;
-private Platform plat2;
-private Platform plat3;
-private Platform plat4;
-private Platform plat5;
-private Platform plat6;
-private Platform plat7;
-
-public void addNotify() {
-	super.addNotify();
-	if (thread == null) {
-	thread = new Thread(this);
-	thread.start();
+	final static int platWidth = 200;
+	final static int platLength = 20;
+	
+	final static int plat1_InitX = 0;
+	final static int plat1_InitY = 0;
+	
+	final static int plat2_InitX = 256;
+	final static int plat2_InitY = 144;
+	
+	final static int plat3_InitX = 2*plat2_InitX;
+	final static int plat3_InitY = 2*plat2_InitY;
+	
+	final static int plat4_InitX = 3*plat2_InitX;
+	final static int plat4_InitY = 3*plat2_InitY;
+	
+	final static int plat5_InitX = 4*plat2_InitX;
+	final static int plat5_InitY = 4*plat2_InitY;
+		
+	final static int plat6_InitX = 5*plat2_InitX;
+	final static int plat6_InitY = 5*plat2_InitY;
+	
+	final static int plat7_InitX = 6*plat2_InitX;
+	final static int plat7_InitY = 6*plat2_InitY;
+		
+	private Platform plat1;
+	private Platform plat2;
+	private Platform plat3;
+	private Platform plat4;
+	private Platform plat5;
+	private Platform plat6;
+	private Platform plat7;
+	
+	
+	private Thread thread;
+	
+	private boolean running;
+	
+	private int FPS = 60;
+	
+	private long targetTime = 1000 / FPS;
+	
+	public GamePanel() {
+		super();
+		setPreferredSize(new Dimension(game.WIDTH, game.HEIGHT));
+		setFocusable(true);
+		requestFocus();
 	}
-	running = true;
-}
-
-
-
-public void init() {
-	plat1 = new Platform(plat1_InitX, plat1_InitY, game.WIDTH, game.HEIGHT);
-	plat2 = new Platform(plat2_InitX, plat2_InitY, game.WIDTH, game.HEIGHT);
-	plat3 = new Platform(plat3_InitX, plat3_InitY, game.WIDTH, game.HEIGHT);
-	plat4 = new Platform(plat4_InitX, plat4_InitY, game.WIDTH, game.HEIGHT);
-	plat5 = new Platform(plat5_InitX, plat5_InitY, game.WIDTH, game.HEIGHT);
-	plat6 = new Platform(plat6_InitX, plat6_InitY, game.WIDTH, game.HEIGHT);
-	plat7 = new Platform(plat7_InitX, plat7_InitY, game.WIDTH, game.HEIGHT);
-	Vector2D displacement = new Vector2D(-16, -9);
-	plat1.setDisplacement(displacement);
-	plat2.setDisplacement(displacement);
-	plat3.setDisplacement(displacement);
-	plat4.setDisplacement(displacement);
-	plat5.setDisplacement(displacement);
-	plat6.setDisplacement(displacement);
-	plat7.setDisplacement(displacement);
-}
-
-public GamePanel() {
-	super();
-	setPreferredSize(new Dimension(game.WIDTH, game.HEIGHT));
-	setFocusable(true);
-	requestFocus();
-}
-
- /*
- * Updates the position of the platforms
- */
-public void update() {
-	plat1.updatePos();
-	plat2.updatePos();
-	plat3.updatePos();
-	plat4.updatePos();
-	plat5.updatePos();
-	plat6.updatePos();
-	plat7.updatePos();
-}
-
-@Override
-public void run() {
-	init();
-	long start;
-	long elapsed;
-	long wait;
-	while (running) {
-		start = System.nanoTime();
-		update();
-		elapsed = System.nanoTime() - start;
-		wait = targetTime - elapsed / 1000000;
-		if (wait < 0 ) wait = 5;
-		try {
-			Thread.sleep(wait);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			repaint();
+	
+	public void addNotify() {
+		super.addNotify();
+		if (thread == null) {
+		thread = new Thread(this);
+		thread.start();
+		}
+		running = true;
 	}
-}
+	
+	
+	
+	public void init() {
+		plat1 = new Platform(plat1_InitX, plat1_InitY, game.WIDTH, game.HEIGHT);
+		plat2 = new Platform(plat2_InitX, plat2_InitY, game.WIDTH, game.HEIGHT);
+		plat3 = new Platform(plat3_InitX, plat3_InitY, game.WIDTH, game.HEIGHT);
+		plat4 = new Platform(plat4_InitX, plat4_InitY, game.WIDTH, game.HEIGHT);
+		plat5 = new Platform(plat5_InitX, plat5_InitY, game.WIDTH, game.HEIGHT);
+		plat6 = new Platform(plat6_InitX, plat6_InitY, game.WIDTH, game.HEIGHT);
+		plat7 = new Platform(plat7_InitX, plat7_InitY, game.WIDTH, game.HEIGHT);
+		Vector2D displacement = new Vector2D(-16, -9);
+		plat1.setDisplacement(displacement);
+		plat2.setDisplacement(displacement);
+		plat3.setDisplacement(displacement);
+		plat4.setDisplacement(displacement);
+		plat5.setDisplacement(displacement);
+		plat6.setDisplacement(displacement);
+		plat7.setDisplacement(displacement);
+	}
+	
 
+	 /*
+	 * Updates the position of the platforms
+	 */
+	public void update() {
+		plat1.updatePos();
+		plat2.updatePos();
+		plat3.updatePos();
+		plat4.updatePos();
+		plat5.updatePos();
+		plat6.updatePos();
+		plat7.updatePos();
+	}
+	
+	@Override
+	public void run() {
+		init();
+		long start;
+		long elapsed;
+		long wait;
+		while (running) {
+			start = System.nanoTime();
+			update();
+			elapsed = System.nanoTime() - start;
+			wait = targetTime - elapsed / 1000000;
+			if (wait < 0 ) wait = 5;
+			try {
+				Thread.sleep(wait);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				repaint();
+		}
+	}
+	
 
 }
